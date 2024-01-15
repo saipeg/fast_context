b4b6a8c233fd21c5e49da78207be1549a9cf11c6
Андрей Согомонян <AASogomonyan@sberbank.ru>
2024-01-15 13:38:36 +0300

    Added bean wiring from config-method with @Bean

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -1,8 +1,27 @@
 package config;
 
+import main.Parrot;
+import main.Person;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ProjectConfig {
+
+    @Bean
+    Parrot parrot() {
+        var p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    Person person() {
+        var p = new Person();
+        p.setName("Ella");
+        p.setParrot(parrot());
+        return p;
+    }
+
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,8 +3,6 @@ package main;
 import config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.util.function.Supplier;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -12,17 +10,11 @@ public class Main {
                 ProjectConfig.class
         );
 
-        Parrot x = new Parrot();
-        x.setName("Kiki");
+        Parrot parrot = context.getBean(Parrot.class);
+        Person person = context.getBean(Person.class);
 
-        Supplier<Parrot> parrotSupplier = () -> x;
-
-        context.registerBean("parrot1",
-                Parrot.class, parrotSupplier);
-
-        Parrot p = context.getBean(Parrot.class);
-
-        System.out.println(p);
-        System.out.println(p.getName());
+        System.out.println("Person's name: " + person.getName());
+        System.out.println("Parrot's name: " + parrot.getName());
+        System.out.println("Person's parrot: " + person.getParrot());
     }
 }
