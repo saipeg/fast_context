f15ea7b463362b750e38bd3354cbb09f9df00377
Андрей Согомонян <AASogomonyan@sberbank.ru>
2024-01-23 21:35:36 +0300

    126 pages

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,20 @@
 package main;
 
-import config.ProjectConfig;
+import configuration.ProjectConfiguration;
+import model.Comment;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
 
 public class Main {
-    public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(
-                ProjectConfig.class
-        );
+  public static void main(String[] args) {
+    var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        Parrot parrot = context.getBean(Parrot.class);
-        Person person = context.getBean(Person.class);
+    var comment = new Comment();
+    comment.setAuthor("Laurentiu");
+    comment.setText("Demo comment");
 
-        System.out.println("Person's name: " + person.getName());
-        System.out.println("Parrot's name: " + parrot.getName());
-        System.out.println("Person's parrot: " + person.getParrot());
-    }
+    var commentService = context.getBean(CommentService.class);
+    commentService.publishComment(comment);
+  }
 }
