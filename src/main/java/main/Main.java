254b39a3c4b15787f6d32f249c0cf3e6e79052d8
Андрей Согомонян <AASogomonyan@sberbank.ru>
2024-01-25 16:46:25 +0300

    Added logs and AOP version 1.0

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,20 @@
 package main;
 
 import config.ProjectConfig;
+import model.Comment;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
 
 public class Main {
-    public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(
-                ProjectConfig.class
-        );
+  public static void main(String[] args) {
+    var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot parrot = context.getBean(Parrot.class);
-        Person person = context.getBean(Person.class);
+    var service = c.getBean(CommentService.class);
 
-        System.out.println("Person's name: " + person.getName());
-        System.out.println("Parrot's name: " + parrot.getName());
-        System.out.println("Person's parrot: " + person.getParrot());
-    }
+    Comment comment = new Comment();
+    comment.setText("Demo comment");
+    comment.setAuthor("Natasha");
+    service.publishComment(comment);
+  }
 }
